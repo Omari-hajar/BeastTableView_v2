b53c4380d807ce381f3362f5a3178edc414f4099
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-12-06 08:15:37 +0300

    Initial Commit

---
+++ BeastTableView_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeastTableView_v2
+//
+//  Created by Hajar Alomari on 06/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
